89385df905f25c4f1593bab6d61165dc483b23d0
James Petersen <james@localhost.localdomain>
2019-07-25 14:33:25 -0400

    updated install script so it can be curled

--- install.sh
+++ install.sh
@@ -1,6 +1,31 @@
+#
+#
+#   Install and configure NeoVim
+#
+#   James Petersen
+#
+#
+
+
+mkdir -p ~/.vim
+
+# Clone the config repo
+if [ ! -d ~/.vim/vim-config ]; then
+    git clone https://github.com/zer0-f0x/vim-config.git
+else
+    echo "vim-config exists"
+fi
 
 cd && ln -s ~/.vim/vim-config/vimrc .vimrc
+
+# Grab vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Set up NeoVim Configuration
 mkdir -p ~/.config/nvim
 cd ~/.config/nvim && ln -s ~/.vim/vim-config/init.vim init.vim
 cd ~/.config/nvim && ln -s ~/.vim/vim-config/settings.json settings.json
 
+# Install and update all plugins
+nvim +PlugInstall +PlugUpdate +qa
